428a8359161331fe64930b7d18864f078a38e7c5
Jastin-vlogger <justinkj765@gmail.com>
2022-10-08 10:52:43 +0530

    google authetication removed

--- login-signUp/server.js
+++ login-signUp/server.js
@@ -46,57 +46,5 @@ app.use('/',admin)
 
 app.listen(port,console.log("http://localhost:3008"))
 
-// index.js
 
-/*  PASSPORT SETUP  */
-const passport = require('passport');
-var userProfile;
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.set('view engine', 'ejs');
-
-app.get('/success', (req, res) =>{
-  console.log(userProfile);
-  res.send(userProfile)
-} );
-app.get('/error', (req, res) => res.send("error logging in"));
-
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-
-// index.js
-
-/*  Google AUTH  */
- 
-const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = '225477678393-rbc9qhsd171ljc7p9infql54fnpm63os.apps.googleusercontent.com';
-const GOOGLE_CLIENT_SECRET = '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';
-passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:3008/auth/google/callback"
-  },
-  function(accessToken, refreshToken, profile, done) {
-      userProfile=profile;
-      return done(null, userProfile);
-  }
-));
- 
-app.get('/auth/google', 
-  passport.authenticate('google', { scope : ['profile', 'email'] }));
- 
-app.get('/auth/google/callback', 
-  passport.authenticate('google', { failureRedirect: '/error' }),
-  function(req, res) {
-    // Successful authentication, redirect success.
-    res.redirect('/success');
-  });
 
